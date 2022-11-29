e1f7947d6ab68aa04cd73b9895ab3a4c66bd7385
sadatyussuf <tommyleespartan0@gmail.com>
2022-11-29 21:56:36 +0000

    running app

--- src/main.py
+++ src/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 
+from .database import connect_to_database
+from .routes import district_capitals
+
+connect_to_database()
 app = FastAPI()
+
+app.include_router(district_capitals.router)
+# connect to database
 
 
 @app.get("/")
