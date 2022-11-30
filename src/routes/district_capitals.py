5511f79cd75b48b7ed87ba38a78b311a0246bea9
sadatyussuf <tommyleespartan0@gmail.com>
2022-11-30 14:18:41 +0000

    added endpoint to get district_capitals by their name

--- src/routes/district_capitals.py
+++ src/routes/district_capitals.py
@@ -1,6 +1,7 @@
 import json
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
+from mongoengine.queryset.visitor import Q
 
 from ..models.district_capitals_model import Capital as Capitals
 
@@ -9,9 +10,33 @@ router = APIRouter(tags=["District Capitals"], prefix="/district_capitals")
 
 @router.get("/")
 def get_all_district_capitals():
+
     response = json.loads(Capitals.objects().to_json())
 
+    if len(response) == 0:
+        raise HTTPException(status_code=status.HTTP_204_NO_CONTENT, detail="Items not found")
+
     return response
+
+
+@router.get("/{name}")
+def get_district_capitals_by_name_or_district(name: str):
+    try:
+        response = json.loads(
+            Capitals.objects(Q(NAMECAP__icontains=name) | Q(DISTRICT__icontains=name)).to_json()
+        )
+        # if len(response) == 0:
+        #     raise HTTPException(
+        #         status_code=status.HTTP_404_NOT_FOUND, detail=f"No Item Found with name {name}"
+        #     )
+
+        return response
+    except AttributeError as e:
+
+        print(e)
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="No Item Found with that name"
+        )
 
 
 @router.get("/{id}")
